0ce629ee69fa8f9e990a068ad2a10209b121ac9f
AlexiFa <113013323+AlexiFa@users.noreply.github.com>
2023-06-07 12:16:37 +0200

    srgz

--- MasterCampGameCode/src/Main.java
+++ MasterCampGameCode/src/Main.java
@@ -1,5 +1,25 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // create map grid with swing
+        JFrame frame = new JFrame("Map");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLayout(new GridLayout(22, 22));
+        JLabel[][] buttons = new JLabel[22][22];
+        for (int i = 0; i < 22; i++) {
+            for (int j = 0; j < 22; j++) {
+                JLabel label = new JLabel();
+                label.setBackground(Color.WHITE);
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                buttons[i][j] = label;
+                buttons[i][j].setBackground(Color.WHITE);
+                frame.add(buttons[i][j]);
+            }
+        }
+        frame.setSize(1000, 800);
+
     }
 }
