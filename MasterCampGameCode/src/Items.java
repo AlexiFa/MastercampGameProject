a9fea01c0ddebf653cca1a231dc647d4c2d0fbf4
Frexium <elliot.gamache@efrei.net>
2023-06-08 11:45:55 +0200

    :D

--- MasterCampGameCode/src/Items.java
+++ MasterCampGameCode/src/Items.java
@@ -1,4 +1,40 @@
 public class Items {
-    private int damage, healing;
+    private int value;
     private String name;
+    private boolean type;
+
+    public Items(int value, String name, boolean type){
+        this.value = value;
+        this.name = name;
+        this.type = type; //si type est vrai, alors l'item heal. Sinon, il s'agit d'une arme
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public boolean getType(){
+        return this.type;
+    }
+
+    public void setValue(int value){
+        this.value = value;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setType(boolean type){
+        this.type = type;
+    }
+
+
+    public void useItem(Player player){
+        if (this.type){
+            player.heal(this.value);
+        } else {
+            player.takeDamage(this.value);
+        }
+    }
 }
--- MasterCampGameCode/src/Player.java
+++ MasterCampGameCode/src/Player.java
@@ -1,9 +1,52 @@
 import java.util.ArrayList;
 
-public class Player {
+public class Player extends Entity{
     private int experience;
     private ArrayList<Items> inventory;
     private Items selectedItem;
 
+    public Player(int hp, String name) {
+        super(hp, 1, name);
+        experience = 0;
+        inventory = new ArrayList<Items>();
+        selectedItem = null;
+    }
 
+    public int getExperience(){
+        return this.experience;
+    }
+    public ArrayList<Items> getInventory(){
+        return this.inventory;
+    }
+    public Items getSelectedItem(){
+        return this.selectedItem;
+    }
+
+    public void setExperience(int experience){
+        this.experience = experience;
+    }
+    public void setSelectedItem(Items selectedItem){
+        this.selectedItem = selectedItem;
+    }
+
+    public void addToInventory(Items item){
+        this.inventory.add(item);
+    }
+    public void removeFromInventory(Items item){
+        this.inventory.remove(item);
+    }
+
+    public void gainExperience(int experience){
+        if (this.experience + experience >= 100){
+            this.experience = (this.experience + experience) - 100;
+            this.levelUp();
+        } else {
+            this.experience += experience;
+        }
+    }
+    public void levelUp(){
+        this.setLevel(this.getLevel() + 1);
+        this.setMaxHp(this.getMaxHp() + 10);
+        this.setHp(this.getMaxHp());
+    }
 }
