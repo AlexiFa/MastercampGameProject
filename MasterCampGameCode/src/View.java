5873ecda7c02a55f97ced3f226a59e889408dfba
loooolooo <127220301+loooolooo@users.noreply.github.com>
2023-06-08 10:29:47 +0200

    tfvtf

---
+++ MasterCampGameCode/src/View.java
@@ -0,0 +1,64 @@
+public class View extends javax.swing.JFrame{
+
+    private Map map = new Map();
+
+    public View(){
+        initComponents();
+        setTitle("Interface Jeu");
+        setLocationRelativeTo(null);
+
+        //JTextArea1.setText(map.toString());
+    }
+
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    private void initComponents(){
+
+        //Zone défilement
+        jScrollPane1 = new javax.swing.JScrollPane();
+        //Zone de texte
+        jTextArea1 = new javax.swing.JTextArea();
+
+        //Lorsque l'on ferme la fenêtre
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextArea1.setColumns(9);
+        jTextArea1.setRows(9);
+        jTextArea1.setFont(new java.awt.Font("Times", 1, 36));
+        jTextArea1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jTextArea1KeyPressed(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTextArea1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 305, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 428, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        pack();
+    }
+
+    private void jTextArea1KeyPressed(java.awt.event.KeyEvent evt) {
+        map.move(evt.getKeyCode());
+        jTextArea1.setText(map.toString());
+    }
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new View().setVisible(true);
+            }
+        });
+    }
+
+}
