7ac6eac25c46386047ca3bab1c4d1e2d31f5d747
loooolooo <127220301+loooolooo@users.noreply.github.com>
2023-06-20 23:30:37 +0200

    new view

--- MasterCampGameCode/src/View.java
+++ MasterCampGameCode/src/View.java
@@ -12,22 +12,28 @@ public class View extends javax.swing.JFrame{
         setTitle("Interface Jeu");
         setLocationRelativeTo(null);
         jTextArea1.setText(map.toString());
+
     }
+
+
 
 
     private void initComponents(){
 
         //Zone défilement
         jScrollPane1 = new javax.swing.JScrollPane();
+
+
         //Zone de texte
         jTextArea1 = new javax.swing.JTextArea();
+
 
         //Lorsque l'on ferme la fenêtre
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jTextArea1.setColumns(9);
         jTextArea1.setRows(9);
-        jTextArea1.setFont(new java.awt.Font("Courier New", 1, 36));
+        jTextArea1.setFont(new java.awt.Font("Courier New", 1, 15));
         jTextArea1.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 jTextArea1KeyPressed(evt);
@@ -39,12 +45,12 @@ public class View extends javax.swing.JFrame{
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 305, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 725, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 428, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
@@ -55,14 +61,12 @@ public class View extends javax.swing.JFrame{
     private void jTextArea1KeyPressed(java.awt.event.KeyEvent evt) {
         map.move(evt.getKeyCode());
         jTextArea1.setText(map.toString());
-
-
-
     }
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+
                 new View().setVisible(true);
             }
         });
