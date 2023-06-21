fddb3dfb0890d54fe2fe55f712b742d309cea674
Frexium <elliot.gamache@efrei.net>
2023-06-21 11:15:25 +0200

    tempSave save

--- MasterCampGameCode/src/View.java
+++ MasterCampGameCode/src/View.java
@@ -1,5 +1,11 @@
 import java.awt.*;
 import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class View extends javax.swing.JFrame{
 
@@ -70,6 +76,69 @@ public class View extends javax.swing.JFrame{
                 new View().setVisible(true);
             }
         });
+    }
+
+
+    public boolean fetchSaveFile(){
+        String filePath = "savefile.txt";
+        Path path = Paths.get(filePath);
+
+        if (!Files.exists(path)) {
+           return false; 
+        }
+
+        try {
+            // Read all lines from the file
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            // Loop over all lines
+            //create all Items
+            int indexAtt = 0, indexDef = 0;
+            for(String line : lines){
+                if(line == "AttackItem"){
+                    indexAtt = lines.indexOf(line);
+                }
+                if(line == "HealItem"){
+                    indexDef = lines.indexOf(line);
+                }
+            }
+            while(indexAtt < indexDef){
+                Items newItem = new Items(Integer.parseInt(lines.get(indexAtt+1)), lines.get(indexAtt), false);
+                indexAtt += 2;
+                Main.attackItemList.add(newItem);
+            }
+            while(indexDef < lines.size()){
+                Items newItem = new Items(Integer.parseInt(lines.get(indexDef+1)), lines.get(indexDef), true);
+                indexDef += 2;
+                Main.healItemList.add(newItem);
+            }
+
+            //create player
+            Player player = new Player(Integer.parseInt(lines.get(2)), lines.get(4));
+            player.setExperience(Integer.parseInt(lines.get(3)));
+            player.setHp(Integer.parseInt(lines.get(1)));
+            //créer une liste inventaire à partir de ce que l'on a sur la ligne 5
+            List<Items> inventory = new ArrayList<>();
+            String[] parts = lines.get(5).split(" ");
+            for (int i = 0; i < parts.length; i++){
+                for(Items item : Main.attackItemList){
+                    if(item.getName() == parts[i]){
+                        inventory.add(item);
+                    }
+                }
+                for(Items item : Main.healItemList){
+                    if(item.getName() == parts[i]){
+                        inventory.add(item);
+                    }
+                }
+            }
+            player.setInventory(inventory);
+            for(Items item : inventory){
+                if 
+            }
+
+        } catch (IOException e) {
+            System.err.println("An error occurred while reading the file: " + e.getMessage());
+        }
     }
 
 }
